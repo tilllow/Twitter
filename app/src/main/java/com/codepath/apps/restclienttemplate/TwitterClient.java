dec2d4b608b18f8b7c7143d6473043a9b32692b8
Ajuisiwon Azantilow <azantilow@fb.com>
2022-06-06 16:31:28 -0700

    This is an updated version of the previous work I had.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -1,11 +1,16 @@
 package com.codepath.apps.restclienttemplate;
 
+import static com.facebook.stetho.inspector.network.PrettyPrinterDisplayType.JSON;
+import static com.github.scribejava.core.model.OAuthConstants.CONSUMER_KEY;
+import static com.github.scribejava.core.model.OAuthConstants.CONSUMER_SECRET;
+
 import android.content.Context;
 
 import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codepath.oauth.OAuthBaseClient;
 import com.github.scribejava.apis.FlickrApi;
+import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.api.BaseApi;
 
 /*
@@ -20,9 +25,11 @@ import com.github.scribejava.core.builder.api.BaseApi;
  * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
  * 
  */
-public class RestClient extends OAuthBaseClient {
-	public static final BaseApi REST_API_INSTANCE = FlickrApi.instance(FlickrApi.FlickrPerm.WRITE); // Change this
-	public static final String REST_URL = "https://api.flickr.com/services"; // Change this, base API URL
+
+
+public class TwitterClient extends OAuthBaseClient {
+	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance(); // Change this
+	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
 	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;       // Change this inside apikey.properties
 	public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET; // Change this inside apikey.properties
 
@@ -32,7 +39,7 @@ public class RestClient extends OAuthBaseClient {
 	// See https://developer.chrome.com/multidevice/android/intents
 	public static final String REST_CALLBACK_URL_TEMPLATE = "intent://%s#Intent;action=android.intent.action.VIEW;scheme=%s;package=%s;S.browser_fallback_url=%s;end";
 
-	public RestClient(Context context) {
+	public TwitterClient(Context context) {
 		super(context, REST_API_INSTANCE,
 				REST_URL,
 				REST_CONSUMER_KEY,
@@ -43,10 +50,12 @@ public class RestClient extends OAuthBaseClient {
 	}
 	// CHANGE THIS
 	// DEFINE METHODS for different API endpoints here
-	public void getInterestingnessList(JsonHttpResponseHandler handler) {
-		String apiUrl = getApiUrl("?nojsoncallback=1&method=flickr.interestingness.getList");
+	public void getHomeTimeline(JsonHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("statuses/home_timeline.json");
 		// Can specify query string params directly or through RequestParams.
 		RequestParams params = new RequestParams();
+		params.put("count",25);
+		params.put("since_id",1);
 		params.put("format", "json");
 		client.get(apiUrl, params, handler);
 	}
@@ -59,4 +68,6 @@ public class RestClient extends OAuthBaseClient {
 	 *    i.e client.get(apiUrl, params, handler);
 	 *    i.e client.post(apiUrl, params, handler);
 	 */
+
+
 }
